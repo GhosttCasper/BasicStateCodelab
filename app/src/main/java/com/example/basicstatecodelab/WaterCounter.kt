571fb73e533f9824342afb61acab5ccc60d86d9b
Vladimir Laptev <v.laptev1998@gmail.com>
2025-02-26 12:43:01 +0200

    rememberSaveable

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,8 +21,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(24.dp)) {
-        Spacer(modifier = Modifier.height(16.dp))
-        var count by remember { mutableIntStateOf(0) }
+        Spacer(modifier = Modifier.height(24.dp))
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
             var showTask by remember { mutableStateOf(true) }
             if (showTask) {
